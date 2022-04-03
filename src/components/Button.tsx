86fc064fb6e955587946ca85432331aed2ba5d19
Oscar Aguilar <aguilop.oscar@gmail.com>
2022-04-03 11:25:21 -0500

    Nuevas props al componente Button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,16 +1,29 @@
 import { FC } from 'react'
 
 interface Props {
-  isPrimary: boolean
+  isPrimary?: boolean
+  className?: string
+  onClickFn?: () => void
 }
 
-export const Button: FC<Props> = ({ children, isPrimary = true }) => {
+export const Button: FC<Props> = ({
+  children,
+  isPrimary = false,
+  className = '',
+  onClickFn,
+}) => {
   return isPrimary ? (
-    <button className="border-2 border-red-500 bg-red-500 text-white px-6 py-2 rounded-md text-1xl font-medium hover:bg-red-600 transition duration-300">
+    <button
+      onClick={onClickFn}
+      className={`border-2 border-red-500 bg-red-500 text-white px-6 py-2 rounded-md text-1xl font-medium hover:bg-red-600 transition duration-300 ${className}`}
+    >
       {children}
     </button>
   ) : (
-    <button className=" border-2 border-red-600 text-red-600 px-6 py-2 rounded-md text-1xl font-medium hover:bg-red-400 hover:text-black transition duration-300">
+    <button
+      onClick={onClickFn}
+      className={`border-2 border-red-600 text-red-600 px-6 py-2 rounded-md text-1xl font-medium hover:bg-red-400 hover:text-black transition duration-300 ${className}`}
+    >
       {children}
     </button>
   )
