678114790af95e088c03f97e29256468fc60265e
Oscar Aguilar <aguilop.oscar@gmail.com>
2022-04-03 11:48:46 -0500

    Creada la página de registro y añadida a las rutas de la aplicación

---
+++ src/pages/RegisterPage.tsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import { Button } from '../components'
+
+export const RegisterPage = () => {
+  return (
+    <div className="w-full flex items-center justify-center mt-40">
+      <div className="bg-white w-96 h-auto rounded-lg pt-8 pb-8 px-8 flex flex-col items-center shadow-2xl">
+        <h3 className="font-bold text-4xl mb-4">
+          Únete a <span className="text-red-600">CraftWorkers</span>
+        </h3>
+        <input
+          type="text"
+          className="w-full h-12 rounded-lg px-4 text-lg bg-gray-100 focus:ring-blue-600 mb-4"
+          placeholder="Nombre..."
+        />
+        <input
+          type="email"
+          className="w-full h-12 rounded-lg px-4 text-lg bg-gray-100 focus:ring-blue-600 mb-4"
+          placeholder="Email"
+        />
+        <input
+          type="password"
+          className="w-full h-12 rounded-lg px-4 text-lg bg-gray-100 focus:ring-blue-600 mb-4"
+          placeholder="Contraseña"
+        />
+        <input
+          type="password"
+          className="w-full h-12 rounded-lg px-4 text-lg bg-gray-100 focus:ring-blue-600 mb-4"
+          placeholder="Repite tu contraseña"
+        />
+        <Button className="mb-4">Registrarme</Button>
+
+
+        <label className="text-gray-800 mb-4">Ó registrate con</label>
+
+        <div className='w-full flex gap-4'>
+          <button className="w-full h-12 rounded-lg bg-red-600 uppercase font-semibold hover:bg-red-700 text-gray-100 transition mb-4">
+            Google
+          </button>
+          <button className="w-full h-12 rounded-lg bg-blue-600 uppercase font-semibold hover:bg-blue-700 text-gray-100 transition mb-4">
+            Facebook
+          </button>
+        </div>
+      </div>
+    </div>
+  )
+}
--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,11 +1,12 @@
 import { Routes, Route, Link } from 'react-router-dom'
-import { HomePage, LoginPage } from '../pages'
+import { HomePage, LoginPage, RegisterPage } from '../pages'
 
 export const AppRouter = () => {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/login" element={<LoginPage />} />
+      <Route path="/register" element={<RegisterPage />} />
     </Routes>
   )
 }
